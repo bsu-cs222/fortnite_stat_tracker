af4bd2f09cf59ab1f55bca6e6fd4ca2cc7132e5c
Zachary Bates <zbates486@gmail.com>
2023-12-05 20:50:09 -0500

    Add test to reach better coverage then refactor names to fit clean code

--- test/stat_tracker_test.dart
+++ test/stat_tracker_test.dart
@@ -3,71 +3,101 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:r6_getter_test/stat_tracker_classes.dart';
 
 void main() {
-  PlayerStatsDecoder decodePlayerStats = PlayerStatsDecoder();
-  File accountFile = File('test/account.json');
-  final String fileContents = accountFile.readAsStringSync();
-  final body = decodePlayerStats.decodeJson(fileContents);
-  AccountSorter sortPlayers = AccountSorter();
-  Player player1 = Player();
-  Player player2 = Player();
-  Player player3 = Player();
+  PlayerStatsDecoder playerStatsDecoder = PlayerStatsDecoder();
+  AccountSorter accountSorter = AccountSorter();
+  FilterHandler playerStatHandler = FilterHandler();
+  Player drewdeshawn = Player();
+  Player realWizard = Player();
+  Player fakePlayerAccount = Player();
 
-  player1.assignAllStats(body);
+  File drewdeshawnAccountFile = File('test/account.json');
+  final String drewdeshawnFileContents =
+      drewdeshawnAccountFile.readAsStringSync();
+  final body = playerStatsDecoder.decodeJson(drewdeshawnFileContents);
+  drewdeshawn.assignAllStats(body);
 
-  File accountFile2 = File('test/account2.json');
-  final String fileContents2 = accountFile2.readAsStringSync();
-  final body2 = decodePlayerStats.decodeJson(fileContents2);
-  player2.assignAllStats(body2);
+  File realWizardAccountFile = File('test/account2.json');
+  final String realWizardFileContents =
+      realWizardAccountFile.readAsStringSync();
+  final body2 = playerStatsDecoder.decodeJson(realWizardFileContents);
+  realWizard.assignAllStats(body2);
 
-  File accountFile3 = File('test/account3.json');
-  final String fileContents3 = accountFile3.readAsStringSync();
-  final body3 = decodePlayerStats.decodeJson(fileContents3);
-  player3.assignAllStats(body3);
+  File fakePlayerAccountFile = File('test/account3.json');
+  final String fakePlayerFileContents =
+      fakePlayerAccountFile.readAsStringSync();
+  final body3 = playerStatsDecoder.decodeJson(fakePlayerFileContents);
+  fakePlayerAccount.assignAllStats(body3);
 
-  List<Player> playerList = [player1, player2, player3];
+  List<Player> listOfFortnitePlayers = [
+    drewdeshawn,
+    realWizard,
+    fakePlayerAccount
+  ];
 
   test('I can get a users account level', () {
-    int level = decodePlayerStats.parseLevel(body);
-    expect(level, 4);
+    int drewdeshawnAccountLevel = playerStatsDecoder.parseLevel(body);
+    expect(drewdeshawnAccountLevel, 4);
   });
 
   test('I can get the users overall number of eliminations', () {
-    int eliminations = decodePlayerStats.parsePlayerKills(body);
-    expect(eliminations, 1488);
+    int realWizardOverallKills = playerStatsDecoder.parsePlayerKills(body2);
+    expect(realWizardOverallKills, 1489);
   });
 
   test(
       'I can get the users number of eliminations in the solo game mode using a list',
       () {
-    List eliminationsList =
-        decodePlayerStats.parsePlayerGamemodeEliminations(body);
-    int eliminations = eliminationsList[0];
+    List drewdeshawnEliminationsList =
+        playerStatsDecoder.parsePlayerGamemodeEliminations(body);
+    int eliminations = drewdeshawnEliminationsList[0];
     expect(eliminations, 551);
   });
 
   test('I can sort a list of player objects based on their overall KD', () {
-    List<Player> sortedPlayerList =
-        sortPlayers.sortAccountListByOverallStat(playerList, "KD");
-    expect(sortedPlayerList[0].username, player3.username);
+    List<Player> sortedListOfFortnitePlayers =
+        accountSorter.sortAccountListByOverallStat(listOfFortnitePlayers, "KD");
+    expect(sortedListOfFortnitePlayers[0].username, fakePlayerAccount.username);
   });
 
   test(
       'I can sort a list of player objects based on their overall eliminations',
       () {
-    List<Player> sortedPlayerList =
-        sortPlayers.sortAccountListByOverallStat(playerList, "eliminations");
-    expect(sortedPlayerList[0].username, player3.username);
+    List<Player> sortedListOfFortnitePlayers = accountSorter
+        .sortAccountListByOverallStat(listOfFortnitePlayers, "eliminations");
+    expect(sortedListOfFortnitePlayers[0].username, fakePlayerAccount.username);
   });
 
-  test('I can sort a list of player objects based on solo KD', () {
-    List<Player> sortedPlayerList =
-        sortPlayers.sortAccountListByGamemodeStat(playerList, "KD", "solo");
-    expect(sortedPlayerList[0].username, player3.username);
+  test('I can sort a list of player objects based on their solo KD', () {
+    List<Player> sortedListOfFortnitePlayers = accountSorter
+        .sortAccountListByGamemodeStat(listOfFortnitePlayers, "KD", "solo");
+    expect(sortedListOfFortnitePlayers[0].username, fakePlayerAccount.username);
+  });
+
+  test('When given an overall stat, I can return the chosen stat', () {
+    playerStatHandler.returnOverallStat(drewdeshawn, 'winRate');
+    expect(drewdeshawn.winRate, 13.0875);
+  });
+
+  test('When given an overall stat, I can return the chosen stat', () {
+    playerStatHandler.returnOverallStat(realWizard, 'matchesPlayed');
+    expect(realWizard.matchesPlayed, 1598);
+  });
+
+  test('When given a specific stat, I can return the chosen stat', () {
+    playerStatHandler.returnSpecificGamemodeStat(
+        fakePlayerAccount, 'eliminations', "squad");
+    expect(fakePlayerAccount.gamemodeEliminationsList[3], 601);
+  });
+
+  test('When given a specific stat, I can return the chosen stat', () {
+    playerStatHandler.returnSpecificGamemodeStat(
+        realWizard, 'matchesPlayed', "solo");
+    expect(fakePlayerAccount.gamemodeMatchesPlayedList[0], 515);
   });
 
   test('I can sort a list of player objects based on trio winrate', () {
-    List<Player> sortedPlayerList = sortPlayers.sortAccountListByGamemodeStat(
-        playerList, "winRate", "trio");
-    expect(sortedPlayerList[1].username, player1.username);
+    List<Player> sortedPlayerList = accountSorter.sortAccountListByGamemodeStat(
+        listOfFortnitePlayers, "winRate", "trio");
+    expect(sortedPlayerList[2].username, fakePlayerAccount.username);
   });
 }
